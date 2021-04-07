0a6d33d58708537e8fc50084b4db46d00fc23737
Michael <ml145616@gmail.com>
2021-04-07 00:00:22 -0400

    Correct typos, remove unused dependencies and refactor variables

--- KosarajuAlgorithm.py
+++ KosarajuAlgorithm.py
@@ -7,7 +7,7 @@ Created on Tue Apr  6 23:41:09 2021
 
 # Use Kosaraju's Algorithm
 # Faster than DFS method
-import itertools
+
 
 class Solution:
     # The following implementation uses Kosaraju's Algorithm
@@ -24,7 +24,7 @@ class Solution:
     def graph_transformation(self, numCourses, prerequisites):
         graph = {_: set() for _ in range(numCourses)}
         for course, prereq in prerequisites:
-            # Due to not gaving the built-in mechanism of deselecting self-contained loops
+            # Due to not having the built-in mechanism of deselecting self-contained loops
             # Like the other implementation
             # The following logic needs to be added in order for this to work
             if course == prereq: 
@@ -35,37 +35,42 @@ class Solution:
         return graph
 
     # This function reverts the edges
-    def tr(self, G):
-        GT = {}
-        for u in G:
-            GT[u] = set()
-        for u in G:
-            for v in G[u]:
-                GT[v].add(u)
-        return GT
+    def edge_reversal(self, graph):
+        reverse_graph = {}
+        for u in graph:
+            reverse_graph[u] = set()
+        for u in graph:
+            for v in graph[u]:
+                reverse_graph[v].add(u)
+        return reverse_graph
 
     # This function traverses through the whole graph barring forbidden/seen regions
-    def walk(self, G, s, S=set()):
-        P, Q = dict(), set()
-        P[s] = None
-        Q.add(s)
-        while Q:
-            u = Q.pop()
-            for v in G[u].difference(P,S):
-                Q.add(v)
-                P[v] = u
-        return P
+    def walk(self, graph, s, seen=set()):
+        if seen is None:
+            seen = set()
+        prev_state, queue = dict(), set()
+        prev_state[s] = None
+        queue.add(s)
+        while queue:
+            u = queue.pop()
+            for v in graph[u].difference(prev_state, seen):
+                queue.add(v)
+                prev_state[v] = u
+        return prev_state
 
     # Topological sort using DFS
-    def dfs_topsort(self, G):
-        S, seq = set(), []
+    def dfs_topsort(self, graph):
+        seen, seq = set(), []
+        
         def recurse(u):
-            if u in S: return
-            S.add(u)
-            for v in G[u]:
+            if u in seen:
+                return
+            seen.add(u)
+            for v in graph[u]:
                 recurse(v)
             seq.append(u)
-        for u in G:
+            
+        for u in graph:
             recurse(u)
         seq.reverse()
         return seq
@@ -75,13 +80,13 @@ class Solution:
     # Step 2: Reverse all edges
     # Step 3: Run full traversal using sequence
     def find_scc(self, numCourses, prerequisites):
-        G = self.graph_transformation(numCourses, prerequisites)
-        GT = self.tr(G)
+        graph = self.graph_transformation(numCourses, prerequisites)
+        reverse_graph = self.edge_reversal(graph)
         sccs, seen = [], set()
-        # print(self.dfs_topsort(G))
-        for u in self.dfs_topsort(G):
-            if u in seen: continue
-            C = self.walk(GT, u, seen)
+        for u in self.dfs_topsort(graph):
+            if u in seen: 
+                continue
+            C = self.walk(reverse_graph, u, seen)
             seen.update(C)
             sccs.append(C)
-        return sccs, self.dfs_topsort(G)
+        return sccs, self.dfs_topsort(graph)
